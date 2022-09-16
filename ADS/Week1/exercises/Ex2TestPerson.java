b9ac7c529d7b8726cc755b3789680fc1a487c478
Ross McMenamin <ross.mcm7@gmail.com>
2022-09-16 13:37:30 +0100

    Exercise2 completed

---
+++ ADS/Week1/exercises/Ex2TestPerson.java
@@ -0,0 +1,11 @@
+
+public class Ex2TestPerson {
+
+	public static void main(String[] args) {
+		//create Person object here
+			Ex2Person s=new Ex2Person();
+			s.displayDetails();
+			s.checkPulse();
+			s.checkForAbuse();
+		}
+}
