56d86d103bab2115162ab19ed56f91023f99ae30
Ross McMenamin <ross.mcm7@gmail.com>
2022-09-17 15:07:11 +0100

    Adding ex1 after completion

--- ADS/Week2/exercises/ex1/KlingonShip.java
+++ ADS/Week2/exercises/ex1/KlingonShip.java
@@ -11,10 +11,22 @@ public class KlingonShip
 	int threatFactor = 7;
 	double cosmicProximity = 20.6;
 	
+	
 	//Put the displayKlingonDetails()and getDangerLeve() methods below.
+	public void displayKlingonDetails()
+	{
+		System.out.println("Power is at " + photonPower);
+		System.out.println("Current Threat Factor is a " + threatFactor);
+		System.out.println("Nearest cosmic entity is " + cosmicProximity + " light years away");
+		System.out.println("Current danger level is estimated at " + getDangerLevel());
+	}
 	
-	
-	
-	
+	public double getDangerLevel()
+	{
+		double dangerLevel;
+		
+		dangerLevel = (photonPower * threatFactor) / cosmicProximity;
+		return dangerLevel;		
+	}
 	
 }
--- ADS/Week2/exercises/ex1/TestKlingonShip.java
+++ ADS/Week2/exercises/ex1/TestKlingonShip.java
@@ -4,7 +4,8 @@ public class TestKlingonShip {
 	public static void main(String[] args) {
 		
 		// Create LingonShip object, then test the methods
-
+		KlingonShip k=new KlingonShip();
+				k.displayKlingonDetails();
 	}
 
 }
