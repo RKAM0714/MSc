a019a5e4b6f2613c4540ac33dc34eb39a8337c8b
Ross McMenamin <ross.mcm7@gmail.com>
2022-09-17 14:52:40 +0100

    Lesson 2 exercises

--- ADS/Week2/Lab1/BankAccount/TestBankAccount.java
+++ ADS/Week2/Lab1/BankAccount/TestBankAccount.java
@@ -3,7 +3,16 @@ public class TestBankAccount {
 
 	public static void main(String[] args) {
 		// Create BanAccount object and test the methods
-
+		BankAccount bank=new BankAccount();
+		
+		bank.setAccountHolder("H");
+		//System.out.println(setAccountHolder);
+		
+		bank.setAccountHolder("A");
+		//System.out.println(setAccountHolder);
+		
+		bank.setAccountHolder("Bruce");
+		//System.out.println(setAccountHolder);
 	}
 
 }
